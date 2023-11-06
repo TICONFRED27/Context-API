bae7aaeebe32c80257e0d569561d01e91ecd264f
FREDERIC TICON <fred.ticon@gmail.com>
2023-11-06 14:33:07 +0100

    Update App.jsx

--- contextApi/src/App.jsx
+++ contextApi/src/App.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import UserContext from "./contexts/UserContext";
 import "./App.css";
 import UserProfile from "./components/UserProfile";
 
 function App() {
-  return <UserProfile />;
+  const [isOnline, setIsOnline] = useState(false);
+
+  return (
+    <UserContext.Provider
+      value={{ isOnline: isOnline, setIsOnline: setIsOnline }}>
+      <UserProfile />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
